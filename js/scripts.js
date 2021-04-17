9976ccbb477906eea79a6cacf29e06914b4c5b91
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-04-17 14:44:52 +0300

    add new changes

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+    $("#show-design").toggle();
+    $(".design-image").toggle();
+})
+$("#development").click(function(){
+    $("#show-development").toggle();
+    $(".development-image").toggle();
+})
+$("#productManagement").click(function(){
+$("#show-product-management").toggle()
+$(".product-management").toggle()
+})
+
+});
